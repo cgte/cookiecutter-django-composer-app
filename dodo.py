145462fcad57af8e23fdf3d0fc92749980024ed6
Colin Goutte <cgte@bk.ru>
2023-05-22 15:37:57 +0200

    [GREEN] use noinput and the script completes

--- dodo.py
+++ dodo.py
@@ -22,7 +22,7 @@ def task_default():
             "echo ran",
             "",
             f"mkdir -p {destination_directory}/{IDENTIFIER}",
-            f"cookiecutter {cookiecutter_source_directory} --output-dir {destination_directory}",
+            f"cookiecutter {cookiecutter_source_directory} --no-input --output-dir {destination_directory} app_name=external_app_name",
         ],
         "verbosity": 2,
         "title": title_with_actions,
