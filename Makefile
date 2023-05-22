25aa8dd7f0894df858b1a1598243231e63810ddf
Colin Goutte <cgte@bk.ru>
2023-05-22 15:22:39 +0200

    Sample scipt is ok about getting information

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 
 tdd:
-	git ls-files |  entr doit
+	git ls-files |  entr make check
+
+check:
+	doit
+	cd /tmp/ ; ls test_pp_* ; rm -r test_pp_*
+	echo "ok"
--- dodo.py
+++ dodo.py
@@ -5,19 +5,25 @@ from pathlib import Path
 
 IDENTIFIER = random.randint(1, 1000)
 
-name = __file__
 
-print(__file__)
-
+cookiecutter_source_directory = f"{Path(__file__).parent!s}"
 
 directory_name = f"test_deploy_{IDENTIFIER}"
 inner_identifier = f"inner_id_{IDENTIFIER}"
 
 
+destination_directory = f"/tmp/test_pp_{inner_identifier}"
+
+
 def task_default():
 
     return {
-        "actions": ["echo ran", f"mkdir -P /tmp/test_pp_{inner_identifier}"],
+        "actions": [
+            "echo ran",
+            "",
+            f"mkdir -p {destination_directory}/{IDENTIFIER}",
+            # f"cookiecutter {cookiecutter_source_directory} --output-dir {destination_directory}",
+        ],
         "verbosity": 2,
         "title": title_with_actions,
     }
